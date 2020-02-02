f387d9509a9ea357b2de47261c1ac80f0db8088b
jared <jared@SAM.PK5001Z>
2020-02-02 09:18:33 -0600

    Hello World

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jared
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        String outputText = "Hello World";
+        
+        System.out.println(outputText);
+    }
+}
